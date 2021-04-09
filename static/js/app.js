1eb00cd32a341cda6eb0eb3286e78a37fb5cc391
Letha Varughese <LV882769@WCUPA.EDU>
2021-04-09 14:06:35 -0400

    submit

--- static/js/app.js
+++ static/js/app.js
@@ -25,22 +25,42 @@ button.on("click",function(){
 
     d3.event.preventDefault();
     d3.select(".button").html("");
-    
+
     var inputElement = d3.select("#datetime");
-    filteredtable(inputElement,"datetime");
+    var cityElement = d3.select("#city");
+    var stateElement = d3.select("#state");
+    var countryElement = d3.select("#country");
+    var shapeElement = d3.select("#shape");
+
+
+    //filteredtable(inputElement,"datetime");
+    //filteredtable(cityElement, "city");
+
+    if(!inputElement.value){
+        filteredtable(inputElement,"datetime")
+    }
+
+    if(!inputElement.value){
+        filteredtable(cityElement,"city")
+    }
+    if(!inputElement.value){
+        filteredtable(stateElement,"state")
+    }
+
+
     
     // get the vlue property of the input element
-    var inputValue = inputElement.property("value");
-    var selectedData = tableData.filter(tableData =>tableData.datetime === inputValue);
+    //var inputValue = inputElement.property("value");
+    //var selectedData = tableData.filter(tableData =>tableData.datetime === inputValue);
     
     // loop through
-    selectedData.forEach((dateData) =>{
-        var tr = tbody.append("tr");
-        Object.entries(dateData).forEach(([key,value]) =>{
-            var td = tbody.append("td");
-            td.text(value);
-        })
-    })
+    //selectedData.forEach((dateData) =>{
+       //var tr = tbody.append("tr");
+        //Object.entries(dateData).forEach(([key,value]) =>{
+            //var td = tbody.append("td");
+           //td.text(value);
+        //})
+    //})
 })
 
 function filteredtable(inputElement,inputType){
