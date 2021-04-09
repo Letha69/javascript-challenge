bf6d46533bd8b1cb45418a6985d458cca87ea98a
Letha Varughese <LV882769@WCUPA.EDU>
2021-04-09 10:39:24 -0400

    append the table row

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
-console.log(tableData);
+//console.log(tableData);
+var tbody = d3.select("tbody");
+//console.log(tableData);
+
+
+// Loop through the data
+
+
+tableData.forEach(ufoSighting =>{
+    console.log(ufoSighting);
+    var tr = tbody.append("tr") // append one table row 'tr'
+    Object.entries(ufoSighting).forEach(function([key,value]){
+        console.log(key,value);
+        var td = tbody.append("td");
+        td.text(value);
+    });
+});
+
+
